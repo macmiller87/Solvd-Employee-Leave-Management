29c9b7db857bc5cf3995bb73048777db064b218d
macmiller87 <macamagolf@gmail.com>
2024-08-21 16:41:44 -0300

    update ci-workflow

--- src/database/service/postgService.js
+++ src/database/service/postgService.js
@@ -1,13 +1,13 @@
 import postgres from 'postgres'; 
 
-const setHost = process.env.POSTGRES_HOST_DOCKER;
+const setHost = process.env.POSTGRES_HOST_LOCAL ?? process.env.POSTGRES_HOST_DOCKER;
 
 const postgresSql = postgres({
     host: setHost,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE_DOCKER,
-    username: process.env.POSTGRES_USER_DOCKER,
-    password: process.env.POSTGRES_PASSWORD_DOCKER
+    database: setHost === "localhost" ? process.env.POSTGRES_DATABASE_LOCAL : process.env.POSTGRES_DATABASE_DOCKER,
+    username: setHost === "localhost" ? process.env.POSTGRES_USER_LOCAL : process.env.POSTGRES_USER_DOCKER,
+    password: setHost === "localhost" ? process.env.POSTGRES_PASSWORD_LOCAL : process.env.POSTGRES_PASSWORD_DOCKER
 });
 
 export default postgresSql;
